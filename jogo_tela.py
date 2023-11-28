38d2fd41788362c758541558a01cc937acb5b229
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-28 19:49:31 -0300

    Adição dificuldade e junção tela início com jogo

---
+++ jogo_tela.py
@@ -0,0 +1,289 @@
+import os
+import pygame
+from pygame import mixer
+from classes import *  
+import random
+
+# Código da tela de início
+pygame.init()
+
+mixer.init() 
+mixer.music.load("imagem/goldeneye.mp3") 
+mixer.music.set_volume(0.7) 
+mixer.music.play()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+screen = pygame.display.set_mode((501, 392))
+
+background = pygame.image.load(r'imagem/Insper-Pong.png')
+
+fps = pygame.time.Clock()
+
+running = True
+
+ButtonGroups = pygame.sprite.Group()
+
+Button_1 = Botao(ButtonGroups)
+Button_1.rect.center = (280, 280)
+
+Button_2 = Botao(ButtonGroups)
+Button_2.rect.center = (280, 280)
+
+while running:
+    fps.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if Button_1.rect.collidepoint(mouse_x, mouse_y):
+                running = False  # Sair do loop da tela de início
+
+    screen.fill((0))
+    screen.blit(background, (0, 0))
+
+    ButtonGroups.update()
+    ButtonGroups.draw(screen)
+
+    pygame.display.update()
+
+pygame.quit()
+
+pygame.init()
+
+WIDTH, HEIGHT = 512, 512
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Insper Pong")
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
+
+SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+WINNING_SCORE = 7
+
+
+class Paddle:
+    VEL = 5.5
+
+    def __init__(self, x, y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, WHITE, (0, 0, 0, 0))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
+a = 5
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
+
+
+class PaddleImage:
+    def __init__(self, x, y, image_path, scale_factor=None):
+        self.x = x
+        self.y = y
+        self.original_image = pygame.image.load(image_path)
+
+        if scale_factor is not None:
+            scaled_width = int(self.original_image.get_width() * scale_factor)
+            scaled_height = int(self.original_image.get_height() * scale_factor)
+            self.image = pygame.transform.scale(self.original_image, (scaled_width, scaled_height))
+        else:
+            self.image = self.original_image
+
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+
+
+screen = pygame.display.set_mode((512, 512))
+
+
+def draw(win, paddles, ball, left_score, right_score, left_paddle_image, right_paddle_image):
+    background = pygame.image.load(r'imagem/mesa pingpong 512x512.png')
+    screen.blit(background, (0, 0))
+
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    win.blit(left_score_text, (WIDTH // 4 - left_score_text.get_width() // 2, 20))
+    win.blit(right_score_text, (WIDTH * (3 / 4) -
+                                right_score_text.get_width() // 2, 20))
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    for i in range(10, HEIGHT, HEIGHT // 20):
+        if i % 2 == 1:
+            continue
+        
+
+    ball.draw(win)
+    left_paddle_image.draw(win)
+    right_paddle_image.draw(win)
+    pygame.display.update()
+
+def efeitos_sonoros():
+    efeito_1 = "Efeitos Sonoros\Ppong1_1.mp3"
+    efeito_2 = "Efeitos Sonoros\Ppong2_1.mp3"
+    efeito_3 = "Efeitos Sonoros\Ppong3_1.mp3"
+    efeitos = [efeito_1,efeito_2,efeito_3]
+    efeito = random.choice(efeitos)
+    return efeito
+
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                mixer.init() 
+                mixer.music.load(efeitos_sonoros()) 
+                mixer.music.set_volume(0.8) 
+                mixer.music.play()
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                mixer.init() 
+                mixer.music.load(efeitos_sonoros()) 
+                mixer.music.set_volume(0.8) 
+                mixer.music.play()
+                ball.x_vel *= -1
+
+                middle_y = right_paddle.y + right_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+
+
+def handle_paddle_movement(keys, left_paddle, right_paddle, left_paddle_image, right_paddle_image):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+        left_paddle_image.y = left_paddle.y  # Ajuste da coordenada y da imagem
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+        left_paddle_image.y = left_paddle.y  # Ajuste da coordenada y da imagem
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+        right_paddle_image.y = right_paddle.y  # Ajuste da coordenada y da imagem
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+        right_paddle_image.y = right_paddle.y  # Ajuste da coordenada y da imagem
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    left_paddle_image = PaddleImage(-30, HEIGHT // 2 - PADDLE_HEIGHT // 2, "imagem/poloni.png", 0.66)
+    right_paddle_image = PaddleImage(WIDTH - 20 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, "imagem/resina.png", 0.66)
+
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
+
+    left_score = 0
+    right_score = 0
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle], ball, left_score, right_score, left_paddle_image, right_paddle_image)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+
+        handle_paddle_movement(keys, left_paddle, right_paddle, left_paddle_image, right_paddle_image)
+
+        ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
+
+        if ball.x < 0:
+            Ball.MAX_VEL += 2.0
+            right_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            Ball.MAX_VEL += 2.0
+            left_score += 1
+            ball.reset()
+
+        won = False
+        if left_score >= WINNING_SCORE:
+            won = True
+            win_text = "Poloni Ganhou!!!"
+        elif right_score >= WINNING_SCORE:
+            won = True
+            win_text = "Resina Ganhou!!!"
+
+        if won:
+            text = SCORE_FONT.render(win_text, 1, WHITE)
+            WIN.blit(text, (WIDTH//2 - text.get_width() //
+                            2, HEIGHT//2 - text.get_height()//2))
+            pygame.display.update()
+            pygame.time.delay(5000)
+            ball.reset()
+            left_paddle.reset()
+            right_paddle.reset()
+            left_paddle_image.y = left_paddle.y  # Resetando a coordenada y da imagem
+            right_paddle_image.y = right_paddle.y  # Resetando a coordenada y da imagem
+            left_score = 0
+            right_score = 0
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
