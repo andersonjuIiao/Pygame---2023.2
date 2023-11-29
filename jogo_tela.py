9129be5ed5967e1a54effda8002b24bcacbde91d
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-29 10:06:21 -0300

    Correções finais v2

--- jogo_tela.py
+++ jogo_tela.py
@@ -7,6 +7,52 @@ import random
 # Código da tela de início
 pygame.init()
 
+mixer.init() 
+mixer.music.load("imagem/goldeneye.mp3") 
+mixer.music.set_volume(0.7) 
+mixer.music.play()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+screen = pygame.display.set_mode((501, 392))
+
+background = pygame.image.load(r'imagem/Insper-Pong.png')
+
+fps = pygame.time.Clock()
+
+running = True
+
+ButtonGroups = pygame.sprite.Group()
+
+Button_1 = Botao(ButtonGroups)
+Button_1.rect.center = (280, 280)
+
+Button_2 = Botao(ButtonGroups)
+Button_2.rect.center = (280, 280)
+
+while running:
+    fps.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if Button_1.rect.collidepoint(mouse_x, mouse_y):
+                running = False  # Sair do loop da tela de início
+
+    screen.fill((0))
+    screen.blit(background, (0, 0))
+
+    ButtonGroups.update()
+    ButtonGroups.draw(screen)
+
+    pygame.display.update()
+
+pygame.quit()
+
+pygame.init()
+
 WIDTH, HEIGHT = 512, 512
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Insper Pong")
